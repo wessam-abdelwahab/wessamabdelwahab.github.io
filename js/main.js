08bef852fb3f853c8af8e4aa9b8ec2758d302114
wessamabdelwahab <abdelwahab.wessam@gmail.com>
2019-11-07 15:12:04 -0500

    NA

--- js/main.js
+++ js/main.js
@@ -19,20 +19,6 @@ function topFunction() {
 }
 
 
-//Prevent hash
-$(document).ready(function(){
-    $('a').on('click', function(event) {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({scrollTop: $(hash).offset().top}, 900);
-    });
-})
-
-
-//Get Hostname
-document.getElementById("hostname").innerHTML= "<strong>Host </strong>" + location.hostname;
-
-
 //Get DateTime
 let date = new Date();  
 let options = {  
