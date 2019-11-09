d66b78f988ad17e2a26b7ca068e6e6e77dbc3c2b
wessamabdelwahab <abdelwahab.wessam@gmail.com>
2019-11-08 23:21:20 -0500

    NA

--- js/main.js
+++ js/main.js
@@ -29,3 +29,30 @@ let options = {
 document.getElementById("DateTime").innerHTML = date.toLocaleTimeString("en-us", options);   
 
 
+function validateForm() {
+  var emailID = document.contact.email.value;
+  atpos = emailID.indexOf("@");
+  dotpos = emailID.lastIndexOf(".");
+
+  if( document.contact.name.value == "" ) {
+     document.forms["contact"]["name"].style.border = "1px solid red";
+     document.getElementById("namemsg").innerHTML = "Please provide your name!";
+     document.contact.name.focus() ;
+     return false;
+  }
+
+  if (atpos < 1 || ( dotpos - atpos < 2 )) {
+    document.forms["contact"]["email"].style.border = "1px solid red";
+    document.getElementById("emailmsg").innerHTML = "Please enter correct email!";
+    document.contact.email.focus() ;
+    return false;
+  }
+
+  if( document.contact.message.value == "" ) {
+     document.forms["contact"]["text"].style.border = "1px solid red";
+     document.getElementById("textmsg").innerHTML = "Please provide your message!";
+     document.contact.message.focus() ;
+     return false;
+  }
+  return( true );
+};
